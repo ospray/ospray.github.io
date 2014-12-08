c085fe253c861911d35c69fdb594c8e734e9e80f
Ingo Wald <ingo.wald@intel.com>
2014-12-08 14:12:23 -0600

    updated demos page

--- makepages.sh
+++ makepages.sh
@@ -2,8 +2,10 @@
 
 for f in `find . -name '*.content'` ; do
     f_base=`basename $f .content`
+		f_dir=`dirname $f`
 
-    cat header.inc >  $f_base.html
-    cat $f         >> $f_base.html
-    cat footer.inc >> $f_base.html
+		echo "writing "$f_dir/$f_base.html
+    cat header.inc >  $f_dir/$f_base.html
+    cat $f         >> $f_dir/$f_base.html
+    cat footer.inc >> $f_dir/$f_base.html
 done
